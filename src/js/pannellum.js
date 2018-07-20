8d725faf983e4597024e4fbdd63c722788a8f95a
Shidi Zhao <shidizhao@outlook.com>
2018-07-20 16:07:43 +0800

    added VR_1

--- src/js/pannellum.js
+++ src/js/pannellum.js
@@ -151,11 +151,11 @@ dragFix.className = 'pnlm-dragfix';
 uiContainer.appendChild(dragFix);
 
 // Display about information on right click
-var aboutMsg = document.createElement('span');
-aboutMsg.className = 'pnlm-about-msg';
-aboutMsg.innerHTML = '<a href="https://pannellum.org/" target="_blank">Pannellum</a>';
-uiContainer.appendChild(aboutMsg);
-dragFix.addEventListener('contextmenu', aboutMessage);
+// var aboutMsg = document.createElement('span');
+// aboutMsg.className = 'pnlm-about-msg';
+// aboutMsg.innerHTML = '<a href="https://pannellum.org/" target="_blank">Pannellum</a>';
+// uiContainer.appendChild(aboutMsg);
+// dragFix.addEventListener('contextmenu', aboutMessage);
 
 // Create info display
 var infoDisplay = {};
@@ -498,6 +498,10 @@ function onImageLoad() {
             dragFix.addEventListener('touchstart', onDocumentTouchStart, false);
             dragFix.addEventListener('touchmove', onDocumentTouchMove, false);
             dragFix.addEventListener('touchend', onDocumentTouchEnd, false);
+            // dragFix.addEventListener('touchend', function (e) {
+            //     e.preventDefault();
+            // }, false);
+
         }
 
         // Deal with MS pointer events
@@ -814,7 +818,7 @@ function onDocumentTouchStart(event) {
 
     // Turn off auto-rotation if enabled
     stopAnimation();
-
+    // 触摸不停止陀螺仪
     stopOrientation();
     config.roll = 0;
 
@@ -881,7 +885,7 @@ function onDocumentTouchMove(event) {
         // we introduce a dynamic pan speed coefficient.
         //
         // Currently this seems to *roughly* keep initial drag/pan start position close to
-        // the user's finger while panning regardless of zoom level / config.hfov value.
+        // the user's finger while panning regardless of zoom level / config.hfov value .
         var touchmovePanSpeedCoeff = (config.hfov / 360) * config.touchPanSpeedCoeffFactor;
 
         var yaw = (onPointerDownPointerX - clientX) * touchmovePanSpeedCoeff + onPointerDownYaw;
@@ -1702,8 +1706,8 @@ function createHotSpot(hs) {
         var image = document.createElement('img');
         image.src = sanitizeURL(p);
         //fix the width of image type to 16:9
-        image.style.width = '720px';
-        //image.style.paddingTop = '405px';
+        image.style.width = '100px';
+        image.style.paddingTop = '5px';
         renderContainer.appendChild(div);
         a.appendChild(image);
         span.style.maxWidth = 'initial';
@@ -1732,7 +1736,13 @@ function createHotSpot(hs) {
 
     if (hs.createTooltipFunc) {
         hs.createTooltipFunc(div, hs.createTooltipArgs);
-    } else if (hs.text || hs.video || hs.image) {
+    } else if (hs.text || hs.image) {
+        div.classList.add('pnlm-tooltip');
+        div.appendChild(span);
+        span.style.width = span.scrollWidth - 20 + 'px';
+        span.style.marginLeft = -(span.scrollWidth - div.offsetWidth) / 2 + 'px';
+        span.style.marginTop = -span.scrollHeight - 350 + 'px';
+    } else if (hs.video) {
         div.classList.add('pnlm-tooltip');
         div.appendChild(span);
         span.style.width = span.scrollWidth - 20 + 'px';
